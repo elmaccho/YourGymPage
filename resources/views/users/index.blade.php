08ad09d3a981dd9b45382a6b9d10486541f1d9aa
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-11-07 21:48:10 +0100

    Users list

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Imię</th>
+            <th scope="col">Nazwisko</th>
+            <th scope="col">Email</th>
+            <th scope="col">Numer telefonu</th>
+            <th scope="col">Typ karnetu</th>
+            <th scope="col">Akcje</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <th scope="row">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->surname }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->phone_number }}</td>
+                    <td>{{ $user->pass_type }}</td>
+                    <td>Mark</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
